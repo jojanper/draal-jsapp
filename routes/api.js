15163899a31a7260b0d4a36b975371515e80eb31
Juha Ojanperä <juha.ojanpera@nokia.com>
2017-03-18 11:04:31 +0200

    Code styling.

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router();
 
-router.get('', function(req, res, next) {
+router.get('', (req, res) => {
   res.json({foo: 'bar'});
 });
 
