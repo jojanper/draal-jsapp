f460738fdfaba8483d5477e1484d2e1eba38bd7b
Juha Ojanperä <juha.ojanpera@gmail.com>
2017-05-17 21:58:13 +0300

    Update TODO and add documentation.

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,23 @@
 module.exports = {
+    /**
+     * Check application development mode status.
+     *
+     * @param {object} app Express application object.
+     *
+     * @returns {boolean} true if development mode enabled, false otherwise
+     */
     isDevelopment(app) {
         return app.get('env') === 'development';
     },
 
+    /**
+     * Set routes for application use.
+     *
+     * @param {object} router Router object
+     * @param {array} routes List of routes to be included for application.
+     *
+     * @returns {object} Router object.
+     */
     setRoutes(router, routes) {
         routes.forEach((route) => {
             router[route.method](route.url, route.fn);
