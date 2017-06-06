63d20d805efbba3a103d3869eab239eb4b14defb
Juha Ojanperä <juha.ojanpera@gmail.com>
2017-06-06 19:55:27 +0300

    Update.

--- src/apps/user/models/user/manager.js
+++ src/apps/user/models/user/manager.js
@@ -15,23 +15,21 @@ class UserManager extends BaseManager {
         this.execute('findOne', {email: user.email}, null, error)
             .then((existingUser) => {
                 if (existingUser) {
-                    return error(new APIError(format('Account with %s email address already exists', user.email)));
+                    throw new APIError(format('Account with %s email address already exists', user.email));
                 }
 
-                user.save().then((savedUser) => {
-                    AccountProfile.manager.createProfile(savedUser)
-                        .then(savedAccountProfile => success(savedAccountProfile))
-                        .catch(err => error(err));
-                })
-                .catch(err => error(err));
-            });
+                return user.save();
+            })
+            .then(savedUser => AccountProfile.manager.createProfile(savedUser))
+            .then(savedAccountProfile => success(savedAccountProfile))
+            .catch(err => error(err));
     }
 
     findLoginUser(email, password, success, error) {
         this.execute('findOne', {email: email.toLowerCase(), active: true}, null, error)
             .then((user) => {
                 if (!user) {
-                    return error(new APIError(`Email ${email} not found`));
+                    throw new APIError(`Email ${email} not found`);
                 }
 
                 user.comparePassword(password, (err, isMatch) => {
@@ -43,7 +41,8 @@ class UserManager extends BaseManager {
 
                     return error(new APIError('Invalid email or password'));
                 });
-            });
+            })
+            .catch(err => error(err));
     }
 }
 
