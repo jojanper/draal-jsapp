1c9cca705a5ac25de5b62b24a06f7faf5746edfd
Juha Ojanperä <juha.ojanpera@gmail.com>
2018-02-25 10:01:35 +0200

    Cleanup.

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,11 +1,5 @@
 const { resolvers: UserResolvers } = require('../apps/user/graphql');
 
-/*
-query {user(id: "5a6a39ed2564ff5365c79817") {
-  email
-}}
-query {users}
-*/
 module.exports = {
     Query: Object.assign({}, UserResolvers.Query)
 };
