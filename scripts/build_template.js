2d0faf0593d0d6f96a6d0757c51950ea98eb180e
Juha Ojanperä <juha.ojanpera@gmail.com>
2018-04-24 17:50:36 +0300

    Update frontend client workflow (#40)

--- scripts/build_template.js
+++ scripts/build_template.js
@@ -1,89 +1,154 @@
 /**
  * Create client template.
  */
-
 const fs = require('fs');
 const path = require('path');
 const format = require('util').format;
+const htmlparser = require("htmlparser2");
 
-const jsOrder = ['polyfills', 'vendor', 'app'];
-const pathPrefix = path.join('angular-app', 'dist');
 
-function removeDuplicates(input) {
-    let output = [];
+/**
+ * Base class for serializing parsed HTML elements.
+ */
+class BaseHtmlElementSerializer {
+    static create(name, attr, content) {
+        const Cls = (name === 'dng-app') ? DngAppSerializer : this;
+        return new Cls(name, attr, content);
+    }
 
-    input.forEach(item => {
-        const prefix = item.split('.')[0];
+    constructor(name, attr, content) {
+        this.name = name;
+        this.attr = attr;
+        this.content = content;
+    }
 
-        let present = false;
-        for (let i = 0; i < output.length; i++) {
-            const prefix2 = output[i].split('.')[0];
-            if (prefix === prefix2) {
-                present = true;
-                break;
-            }
+    serialize() {
+        // Name of element
+        let html = format('%s(', this.name);
+
+        // Element attributes
+        for (const attr in this.attr) {
+            html = format('%s%s=\'%s\' ', html, attr, this.attr[attr]);
         }
 
-        if (!present) {
-            output.push(item);
+        // Element content
+        if (this.content) {
+            html = format('%s\'%s\'', html, this.content);
         }
-    })
 
-    return output;
-}
+        // Close element
+        html += ')';
 
-// Determine CSS files
-const cssFiles = removeDuplicates(fs.readdirSync(path.join(pathPrefix, 'css'))
-.map(file => {
-    return {
-        name: file,
-        time: fs.statSync(path.join(pathPrefix, 'css', file)).mtime.getTime()
-    };
-})
-.filter(file => file.name.endsWith('.css'))
-.sort((file1, file2) => { return file2.time - file1.time; })
-.map(file => `/css/${file.name}`));
-
-// Determine JS bundle files
-const jsFilesOrig = removeDuplicates(fs.readdirSync(path.join(pathPrefix, 'js'))
-.map(file => {
-    return {
-        name: file,
-        time: fs.statSync(path.join(pathPrefix, 'js', file)).mtime.getTime()
-    };
-})
-.filter(file => file.name.endsWith('.js'))
-.sort((file1, file2) => { return file2.time - file1.time; })
-.map(file => `/js/${file.name}`));
-
-// Correct order for js files
-const jsFiles = [];
-for (let i = 0; i < jsOrder.length; i++) {
-    for (let j = 0; j < jsFilesOrig.length; j++) {
-        if (jsFilesOrig[j].indexOf(jsOrder[i]) > -1) {
-            jsFiles.push(jsFilesOrig[j]);
-        }
+        return html;
     }
 }
 
-const buildFiles = [].concat(cssFiles).concat(jsFiles);
+/**
+ * Serialize dng-app element.
+ */
+class DngAppSerializer extends BaseHtmlElementSerializer {
+    constructor(name, attr, content) {
+        super(name, attr, content);
+    }
 
+    serialize() {
+        return format('<%s>%s</%s>', this.name, this.content, this.name);
+    }
+}
+
+/**
+ * Store target element data.
+ */
+class HtmlData {
+    constructor() {
+        this.name = '';
+        this.attr = '';
+        this.data = [];
+    }
+
+    setParentTag(tag, attr) {
+        this.name = tag;
+        this.attr = attr;
+    }
+
+    setData(tag, attr) {
+        const data = {};
+        data.tag = tag;
+        data.attr = attr;
+        this.data.push(data);
+    }
+
+    setContent(content) {
+        const index = this.data.length - 1;
+        this.data[index].content = content;
+    }
+}
+
+// Parse these elements from frontend template
+const parseTags = ['head', 'body'];
+
+// And save the parsed data here (array of HtmlData items)
+const parsedData = [];
+
+// Final template data for backend
 const template = [
     'extends layout',
-    '',
-    'block content'
+    ''
 ];
 
-let index = 0;
-if (cssFiles.length) {
-    index++;
-    template.push(format('  link(rel="stylesheet" href="%s")', buildFiles[0]));
-}
 
-template.push('  <dng-app>Loading...</dng-app>');
+let targetIndex = 0;
+let currentObj = null;
 
-for (let i = index; i < index + 3; i++) {
-    template.push(format('  script(type="text/javascript" src="%s")', buildFiles[i]));
-}
+const parser = new htmlparser.Parser({
+	onopentag: (name, attribs) => {
+        // Append element to current object
+        if (currentObj) {
+            currentObj.setData(name, attribs);
+        } else if (targetIndex < parseTags.length && parseTags[targetIndex] === name) {
+            // Create new object for storing the parsed data
+            currentObj = new HtmlData();
+            currentObj.setParentTag(name, attribs);
+        }
+    },
+	ontext: (text) => {
+        currentObj.setContent(text);
+    },
+	onclosetag: (tagname) => {
+        // Finished parsing the target element
+        if (parseTags[targetIndex] === tagname) {
+            targetIndex++;
+            parsedData.push(currentObj);
+            currentObj = null;
+        }
+    },
+    onend: () => {
+        // Serialize the data for each target element
+        for (const item of parsedData) {
+            template.push(`block ${item.name}`);
+            for (const child of item.data) {
+                const obj = BaseHtmlElementSerializer.create(child.tag, child.attr, child.content);
+                template.push(`  ${obj.serialize()}`);
+            }
+        }
 
-fs.writeFile(path.join('views', 'index.pug'), template.join('\n'));
+        // Write the template
+        fs.writeFile(path.join('views', 'index.pug'), template.join('\n'), (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        });
+    }
+}, {decodeEntities: true});
+
+// Read the frontend template and parse the data
+const filepath = path.join('public', 'frontend', 'index.html');
+fs.readFile(filepath, (err, data) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    // Pass data to HTML parser
+    parser.write(data.toString());
+    parser.end();
+});
