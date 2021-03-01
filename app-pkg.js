2f8d0a826e634529ceb6cbfbabd7f66be59f987e
Juha Ojanperä <juha.ojanpera@gmail.com>
2021-03-01 18:27:35 +0200

    Update.

--- app-pkg.js
+++ app-pkg.js
@@ -5,6 +5,5 @@ const { spawn } = require('child_process');
 // spawn(process.execPath, [path.join(__dirname, 'app.js')], { stdio: 'inherit' });
 
 // Start node application and detach spawned child process (no terminal remains open)
-const options = { detached: false, stdio: 'ignore' };
-const child = spawn(process.execPath, [path.join(__dirname, 'app.js')], options);
-child.unref();
+const options = { detached: true, stdio: 'ignore' };
+spawn(process.execPath, [path.join(__dirname, 'app.js')], options).unref();
